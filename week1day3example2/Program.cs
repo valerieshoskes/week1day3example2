6623d0394b34d29ccbd72a4999c1ac3610d00db9
GregMahring <mahring190@aol.com>
2016-09-08 13:58:18 -0400

    Bill problem

--- week1day3example2/Program.cs
+++ week1day3example2/Program.cs
@@ -10,9 +10,17 @@ namespace week1day3example2
     {
         static void Main(string[] args)
         {
+            /*
             int totalSugar = 7;
             int currentSugar = 2;
             Console.WriteLine("We need {0} cups of sugar", (totalSugar - currentSugar));
+            */
+
+            int individualPayment = 13;
+            int billSplitBetween = 4;
+
+            Console.WriteLine("The total bill was {0}", (individualPayment * billSplitBetween));
+
         }
     }
 }
