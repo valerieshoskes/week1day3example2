58e01b401cbfa64a36b35abcde1baf00b5d8d0e4
valerieshoskes <indi.vs@gmail.com>
2016-09-08 14:14:14 -0400

    diaper budget problem solved

--- week1day3example2/Program.cs
+++ week1day3example2/Program.cs
@@ -14,13 +14,18 @@ namespace week1day3example2
             int totalSugar = 7;
             int currentSugar = 2;
             Console.WriteLine("We need {0} cups of sugar", (totalSugar - currentSugar));
-            */
+            
 
             int individualPayment = 13;
             int billSplitBetween = 4;
 
             Console.WriteLine("The total bill was {0}", (individualPayment * billSplitBetween));
+            */
 
+            int totalBudget = 40;
+            int singlePackageCost = 8;
+
+            Console.WriteLine("We can buy {0} packs of diapers.", (totalBudget / singlePackageCost));
         }
     }
 }
